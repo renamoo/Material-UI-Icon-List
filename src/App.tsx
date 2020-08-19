50ff69794c8be07a74c0f198482ab2babb3d00c6
renamoo <renamoo808@gmail.com>
2020-08-19 21:54:31 +0900

    #8 remove unused lines

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import * as uiIcons from '@material-ui/icons';
 import React, { Suspense, useState } from 'react';
 import './App.css';
 import Header from './components/Header';
@@ -8,7 +7,6 @@ const Content = React.lazy(() => import('./components/Content'));
 
 const App: React.FC = () => {
   const [select, setSelect] = useState('');
-  const names = Object.keys(uiIcons);
 
   return (
     <div className="App">
